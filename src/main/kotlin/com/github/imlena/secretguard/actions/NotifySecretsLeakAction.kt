11d00f4a0d7fe7e0a829ab869dbed4e54bed9beb
Timofeev Kirill <kirill.timofeev.a@gmail.com>
2024-04-01 16:05:12 +0300

    SecretGuard #5 Add NotifySecretsLeakAction; Replace StringBundle

---
+++ src/main/kotlin/com/github/imlena/secretguard/actions/NotifySecretsLeakAction.kt
@@ -0,0 +1,46 @@
+package com.github.imlena.secretguard.actions
+
+import com.github.imlena.secretguard.StringBundle
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowManager
+
+const val NOTIFICATION_GROUP_ID = "SecretGuardNotificationGroup"
+
+class NotifySecretsLeakAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        notifySecretsLeak(
+                project = project,
+                leaksCount = 3,
+                onReviewButtonClick = {
+                    val toolWindowManager = project?.let { ToolWindowManager.getInstance(it) }
+                    val todoToolWindow = toolWindowManager?.getToolWindow("MyToolWindow")
+                    todoToolWindow?.activate(null)
+                }
+        )
+    }
+
+    private fun notifySecretsLeak(project: Project?, leaksCount: Int, onReviewButtonClick: () -> Unit) {
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup(NOTIFICATION_GROUP_ID)
+                .createNotification(
+                        title = StringBundle.string("secretsLeakBalloonTitle"),
+                        content = StringBundle.string("secretsLeakBalloonDescription", leaksCount),
+                        type = NotificationType.INFORMATION
+                )
+                .addAction(NotificationAction.createSimpleExpiring(StringBundle.string("secretsLeakBalloonPositiveButton")) {
+                    thisLogger().info("Review secrets clicked")
+                    onReviewButtonClick()
+                })
+                .addAction(NotificationAction.createSimpleExpiring(StringBundle.string("secretsLeakBalloonDismissButton")) {
+                    thisLogger().info("Ignore clicked")
+                })
+                .notify(project)
+    }
+}
--- src/main/kotlin/com/github/imlena/secretguard/services/MyProjectService.kt
+++ src/main/kotlin/com/github/imlena/secretguard/services/MyProjectService.kt
@@ -1,15 +1,15 @@
 package com.github.imlena.secretguard.services
 
+import com.github.imlena.secretguard.StringBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.imlena.secretguard.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
 
     init {
-        thisLogger().info(MyBundle.message("projectService", project.name))
+        thisLogger().info(StringBundle.string("projectService", project.name))
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
