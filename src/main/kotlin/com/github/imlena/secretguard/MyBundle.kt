3286d6a7effaedce05eec2ef44ab63859fc21341
GitHub Action <action@github.com>
2024-04-01 08:23:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/imlena/secretguard/MyBundle.kt
+++ src/main/kotlin/com/github/imlena/secretguard/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.imlena.secretguard
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/imlena/secretguard/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/imlena/secretguard/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.imlena.secretguard.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
