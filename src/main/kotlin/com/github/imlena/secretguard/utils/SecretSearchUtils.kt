0f8e8e7e28a245670ff05e45b2abd1d2722a0ce5
Aleksey Shik <alexey.shik@corp.mail.ru>
2024-04-04 02:03:07 +0300

    update regex

--- src/main/kotlin/com/github/imlena/secretguard/utils/SecretSearchUtils.kt
+++ src/main/kotlin/com/github/imlena/secretguard/utils/SecretSearchUtils.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
 fun getSearchRegex(): Regex {
-    return "^(?i)(pass|login|api_key).*[=:].*['\"].*['\"].*".toRegex()
+    return ".*(?i)(pass|login|api_key).*[=:].*['\"].*['\"].*".toRegex()
 }
 
 fun filterConfigFiles(file: VirtualFile?): Boolean {
